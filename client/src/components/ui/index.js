aa42ade6ee1de89865e9949e9fecbec3ef810a76
senkyART <nadaazamti@gmail.com>
2026-05-26 19:55:26 +0100

    add composant that show itineraire day by day

--- client/src/components/ui/index.js
+++ client/src/components/ui/index.js
@@ -14,3 +14,4 @@ export { default as Modal   } from "./Modal";
 export { default as Badge   } from "./Badge";
 export { default as Spinner } from "./Spinner";
 export { default as Pill    } from "./Pill";
+export { default as ItineraireJourJour } from "./ItineraireJourJour";
