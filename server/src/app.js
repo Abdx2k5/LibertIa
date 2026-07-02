b6c6fc19d7a39bdbfd20545eb7c4206256ef9494
Abdelwahab Messoussi <messoussiabdelwahab5@gmail.com>
2026-07-02 22:18:06 +0100

    fix vercel

--- server/src/app.js
+++ server/src/app.js
@@ -28,11 +28,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "https://libertia-production.up.railway.app",
-        /\.vercel\.app$/
-    ],
+    origin: true,
     credentials: true
 }));
 
@@ -87,7 +83,7 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: true,
         credentials: true
     }
 });
